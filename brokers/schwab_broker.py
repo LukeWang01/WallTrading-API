941b246f96ccf94e5f3fc879172ab5b1265bc7b7
angustar <zhaohongzhi@angustar.com>
2024-12-10 00:33:46 +0800

    Adapt to Schwab

--- brokers/schwab_broker.py
+++ brokers/schwab_broker.py
@@ -1,10 +1,33 @@
-from typing import Dict
-from schwab import auth, client
-
 from brokers.base_broker import BaseBroker
+from schwab.auth import easy_client, client_from_login_flow, client_from_manual_flow
+from schwab.orders.equities import equity_buy_limit, equity_sell_limit, equity_buy_market, equity_sell_market
+from schwab.orders.common import Duration, Session
+
+from env._secrete import Schwab_account_number, Schwab_app_key, Schwab_secret
+
+import httpx
+import time
 
 """ ⬇️ Broker Setup ⬇️ """
-# refill the setup like MooMooFutuBroker Class
+'''
+Step 1: Set up the environment information
+'''
+# Environment Variables
+SCHWAB_CALLBACK_URL = 'https://127.0.0.1:8182'  # should be same as the Callback URL of the App in Schwab Developer Portal, just keep as default
+SCHWAB_TOKEN_PATH = './env/_schwab_token.json'
+
+'''
+Step 2: Set up the account information
+'''
+SCHWAB_ACCOUNT_NUMBER = Schwab_account_number  # set up the account number in the env/_secrete.py file
+SCHWAB_APP_KEY = Schwab_app_key  # set up the App Key in the env/_secrete.py file
+SCHWAB_SECRET = Schwab_secret  # set up the Secret in the env/_secrete.py file
+
+'''
+Step 3: Set up the trading information
+'''
+FILL_OUTSIDE_MARKET_HOURS = True  # enable if order fills on extended hours
+
 """ ⏫ Broker Setup ⏫ """
 
 
@@ -12,132 +35,163 @@ class SchwabBroker(BaseBroker):
 
     def __init__(self):
         super().__init__()
-        self.client = None
+        self.connection_attempts = 0
         self.max_attempts = 3
         self.retry_delay = 5  # seconds
 
-    def connect(self, credentials: Dict) -> bool:
-        # test example code, need to be modified
-        # try:
-        #     self.client = Schwab()
-        #     # Login using credentials
-        #     login_successful = self.client.login(
-        #         username=credentials.get('username'),
-        #         password=credentials.get('password'),
-        #         # Optional MFA callback function
-        #         mfa_callback=credentials.get('mfa_callback')
-        #     )
-        #
-        #     if login_successful:
-        #         self.connected = True
-        #         self.logger.info("Successfully connected to Schwab")
-        #         return True
-        #     else:
-        #         self.logger.error("Failed to connect to Schwab")
-        #         return False
-        #
-        # except Exception as e:
-        #     self.logger.error(f"Failed to connect to Schwab: {e}")
-        #     return False
-        pass
+    def connect(self) -> bool:
+        while self.connection_attempts < self.max_attempts:
+            try:
+                self.client = easy_client(api_key=SCHWAB_APP_KEY, app_secret=SCHWAB_SECRET,
+                                    callback_url=SCHWAB_CALLBACK_URL, token_path=SCHWAB_TOKEN_PATH,
+                                    max_token_age=60*60*24*6.5)
+                self.account_hash = self._get_account_hash()[1]
+                self.connected = True
+                self.connection_attempts = 0
+                self.logger.info("Successfully connected to Schwab")
+                return True
+            
+            except Exception as e:
+                self.connection_attempts += 1
+                self.logger.error(
+                    f"Connection attempt {self.connection_attempts} failed: {e}")
+                if self.connection_attempts < self.max_attempts:
+                    time.sleep(self.retry_delay)
+
+        self.logger.error("Failed to connect to Schwab after maximum attempts")
+        return False
+    
+    def _get_account_hash(self):
+        resp = self.client.get_account_numbers()
+        if resp.status_code != httpx.codes.OK:
+            self.logger.error(f"Trader: Get Account Hash failed: {resp.json()['message']}")
+            print(f"Trader: Get Account Hash failed: {resp.json()['message']}")
+            return resp.status_code, None
+
+        account_hash = next(
+            (item['hashValue'] for item in resp.json() if item['accountNumber'] == SCHWAB_ACCOUNT_NUMBER),
+            None
+        )
+        
+        if account_hash:
+            return resp.status_code, account_hash
+        else:
+            self.logger.error('Trader: Get Account Hash failed: wrong account number')
+            print("Trader: Get Account Hash failed: wrong account number")
+            return  -1, None
 
     def get_account_info(self):
-        pass
+        resp = self.client.get_account(self.account_hash)
 
-    def market_sell(self, stock: str, quantity: int, price: float):
-        try:
-            order = self.client.place_order(
-                stock=stock,
-                quantity=quantity,
-                order_type='market',
-                side='sell'
-            )
-            print('Trader: Market Sell success!')
+        if resp.status_code != httpx.codes.OK:
+            self.logger.error(f"Trader: Get Account Info failed: {resp.json()['message']}")
+            print(f"Trader: Get Account Info failed: {resp.json()['message']}")
+            return resp.status_code, resp.json()['message']
+        
+        account_info = resp.json()
+    
+        if account_info['securitiesAccount']['type'] == 'CASH':
+            cash = account_info['securitiesAccount']['currentBalances']['totalCash'] - account_info['securitiesAccount']['currentBalances']['unsettledCash']
+        elif account_info['securitiesAccount']['type'] == 'MARGIN':
+            cash = account_info['securitiesAccount']['currentBalances']['cashBalance']
+        total_assets = account_info['securitiesAccount']['currentBalances']['liquidationValue']
+        market_value = account_info['securitiesAccount']['currentBalances']['longMarketValue']
 
-            return order
+        acct_info = {
+            'cash': round(cash, 2),
+            'total_assets': round(total_assets, 2),
+            'market_value': round(market_value, 2),
+        }
 
-        except Exception as e:
-            self.logger.error(f"Error placing market order: {e}")
-            print('Trader: Market Sell failed: ', e)
-
-    def market_buy(self, stock: str, quantity: int, price: float):
-        try:
-            order = self.client.place_order(
-                stock=stock,
-                quantity=quantity,
-                order_type='market',
-                side='buy'
-            )
-            print('Trader: Market Buy success!')
-
-            return order
-
-        except Exception as e:
-            self.logger.error(f"Error placing market order: {e}")
-            print('Trader: Market Buy failed: ', e)
-
-    def limit_sell(self, stock: str, quantity: int, price: float):
-        try:
-            order = self.client.place_order(
-                stock=stock,
-                quantity=quantity,
-                order_type='limit',
-                side='sell',
-                price=price,
-                extended_hours=True  # add this to setting on top like MooMoo
-            )
-            print('Trader: Limit Sell success!')
-            return order
-
-        except Exception as e:
-            self.logger.error(f"Error placing limit order: {e}")
-            print('Trader: Limit Sell failed: ', e)
-
-    def limit_buy(self, stock: str, quantity: int, price: float):
-        try:
-            order = self.client.place_order(
-                stock=stock,
-                quantity=quantity,
-                order_type='limit',
-                side='buy',
-                price=price,
-                extended_hours=True  # add this to setting on top like MooMoo
-            )
-            print('Trader: Limit Buy success!')
-            return order
-
-        except Exception as e:
-            self.logger.error(f"Error placing limit order: {e}")
-            print('Trader: Limit Buy failed: ', e)
+        return resp.status_code, acct_info
 
     def get_positions(self):
+        resp = self.client.get_account(self.account_hash, fields=[self.client.Account.Fields.POSITIONS])
+
+        if resp.status_code != httpx.codes.OK:
+            self.logger.error(f"Trader: Get Positions failed: {resp.json()['message']}")
+            print(f"Trader: Get Positions failed: {resp.json()['message']}")
+            return resp.status_code, resp.json()['message']
+        else:
+            account_info = resp.json()
+            positions = account_info.get('securitiesAccount', {}).get('positions', [])
+            data_dict = {}
+            for position in positions:
+                code = position['instrument']['symbol']
+                position_data = {key: value for key, value in position.items() if key != 'instrument'}
+                position_data.update(position['instrument'])
+                data_dict[code] = position_data
+            return resp.status_code, data_dict
+
+    def get_cash_balance(self):
+        acct_ret, acct_info = self.get_account_info()
+        if acct_ret == httpx.codes.OK:
+            return acct_info['cash']
+        else:
+            self.logger.error(f"Trader: Get Cash Balance failed: {acct_info}")
+            print("Trader: Get Cash Balance failed: {acct_info}")
+            return -1
+
+    def market_sell(self, stock: str, quantity: int):
+        order = equity_sell_market(stock, quantity).build()
+        resp = self.client.place_order(self.account_hash, order)
+
+        if resp.status_code != httpx.codes.CREATED:
+            self.logger.error(f"Trader: Market Sell failed: {resp.json()['message']}")
+            print(f"Trader: Market Sell failed: {resp.json()['message']}")
+            return resp.status_code, resp.json()['message']
+
+        return resp.status_code, None
+
+    def market_buy(self, stock: str, quantity: int):
+        order = equity_buy_market(stock, quantity).build()
+        resp = self.client.place_order(self.account_hash, order)
+
+        if resp.status_code != httpx.codes.CREATED:
+            self.logger.error(f"Trader: Market Buy failed: {resp.json()['message']}")
+            print(f"Trader: Market Buy failed: {resp.json()['message']}")
+            return resp.status_code, resp.json()['message']
+
+        return resp.status_code, None
+
+    def limit_sell(self, stock: str, quantity: int, price: str):
+        if FILL_OUTSIDE_MARKET_HOURS:
+            order = equity_sell_limit(stock, quantity, price).set_duration(Duration.GOOD_TILL_CANCEL).set_session(
+                    Session.SEAMLESS).build()
+        else:
+            order = equity_sell_limit(stock, quantity, price).set_duration(Duration.GOOD_TILL_CANCEL).set_session(
+                    Session.NORMAL).build()
+        resp = self.client.place_order(self.account_hash, order)
+
+        if resp.status_code != httpx.codes.CREATED:
+            self.logger.error(f"Trader: Limit Sell failed: {resp.json()['message']}")
+            print(f"Trader: Limit Sell failed: {resp.json()['message']}")
+            return resp.status_code, resp.json()['message']
+
+        return resp.status_code, None
+
+    def limit_buy(self, stock: str, quantity: int, price: str):
+        if FILL_OUTSIDE_MARKET_HOURS:
+            order = equity_buy_limit(stock, quantity, price).set_duration(Duration.GOOD_TILL_CANCEL).set_session(
+                    Session.SEAMLESS).build()
+        else:
+            order = equity_buy_limit(stock, quantity, price).set_duration(Duration.GOOD_TILL_CANCEL).set_session(
+                    Session.NORMAL).build()
+        resp = self.client.place_order(self.account_hash, order)
+
+        if resp.status_code != httpx.codes.CREATED:
+            self.logger.error(f"Trader: Limit Buy failed: {resp.json()['message']}")
+            print(f"Trader: Limit Buy failed: {resp.json()['message']}")
+            return resp.status_code, resp.json()['message']
+
+        return resp.status_code, None
+
+    def refresh_token(self):
         try:
-            positions_data = self.client.get_positions()
-            positions = []
-
-            for pos in positions_data:
-                positions.append(pos)  # rewrite pos to access the data
-
-            return positions
-
-        except Exception as e:
-            self.logger.error(f"Error fetching positions: {e}")
-
-    def get_cash_balance(self) -> float:
-        try:
-            account_data = self.client.get_account_info()
-            return float(account_data['cash_balance'])
-
-        except Exception as e:
-            self.logger.error(f"Error fetching cash balance: {e}")
-
-    def get_order_status(self, order_id: str):
-        # try:
-        #     order_status = self.client.get_order_status(order_id)
-        #     print('Trader: Order status: ', order_status)
-        #     return order_status
-        #
-        # except Exception as e:
-        #     self.logger.error(f"Error fetching order status: {e}")
-        #     print('Trader: Error fetching order status: ', e)
-        pass
+            client_from_login_flow(api_key=SCHWAB_APP_KEY, app_secret=SCHWAB_SECRET,
+                                    callback_url=SCHWAB_CALLBACK_URL, token_path=SCHWAB_TOKEN_PATH,
+                                    requested_browser=None, callback_timeout=300)
+        except:
+            print('Failed to fetch a token using a web browser, falling back to the manual flow')
+            client_from_manual_flow(api_key=SCHWAB_APP_KEY, app_secret=SCHWAB_SECRET,
+                                    callback_url=SCHWAB_CALLBACK_URL, token_path=SCHWAB_TOKEN_PATH)
--- env/_secrete.py
+++ env/_secrete.py
@@ -8,6 +8,11 @@ Important, please don't share with the public
 # trading password, 6 digits
 MooMoo_PWD = '123456'   # replace with your moomoo trading password
 
+""" Schwab trading setup """
+Schwab_account_number = '12345678'
+Schwab_app_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+Schwab_secret = 'xxxxxxxxxxxxxxxx'
+
 """ ‼️‼️‼️  please keep your password in SAFE, DO NOT SHARE ‼️‼️‼️"""
 
 # Example credentials.yml structure:
